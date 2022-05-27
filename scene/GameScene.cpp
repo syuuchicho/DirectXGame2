b74b932274b80ffc41ab2426dcbce30e07fcc2df
syuuchicho <k021g1478@g.neec.ac.jp>
2022-05-27 15:26:09 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,199 @@
+﻿#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include"AxisIndicator.h"
+#include"PrimitiveDrawer.h"
+#include<random>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {
+	delete model_;
+	delete debugCamera_;
+}
+
+//度数法
+float Angle(float const &n)
+{
+	return n*3.14/180;
+}
+
+//スケーリング行列
+Matrix4 CreateMatScale(Vector3 scale) {
+	Matrix4 matScale;
+	matScale.m[0][0] = scale.x;
+	matScale.m[1][1] = scale.y;
+	matScale.m[2][2] = scale.z;
+	matScale.m[3][3] = 1.0f;
+
+	return matScale;
+}
+
+//回転行列
+Matrix4 CreateMatRot(Vector3 rotation) {
+	//合成用回転行列を宣言
+	Matrix4 matRot;
+	//各軸用回転行列を宣言
+	Matrix4 matRotX, matRotY, matRotZ;
+
+	matRotZ.m[0][0] = cos(rotation.z);
+	matRotZ.m[0][1] = sin(rotation.z);
+	matRotZ.m[1][0] = -sin(rotation.z);
+	matRotZ.m[1][1] = cos(rotation.z);
+	matRotZ.m[2][2] = 1;
+	matRotZ.m[3][3] = 1;
+
+	matRotX.m[0][0] = 1;
+	matRotX.m[1][1] = cos(rotation.x);
+	matRotX.m[1][2] = sin(rotation.x);
+	matRotX.m[2][1] = -sin(rotation.x);
+	matRotX.m[2][2] = cos(rotation.x);
+	matRotX.m[3][3] = 1;
+
+	matRotY.m[0][0] = cos(rotation.y);
+	matRotY.m[0][2] = -sin(rotation.y);
+	matRotY.m[1][1] = 1;
+	matRotY.m[2][0] = sin(rotation.y);
+	matRotY.m[2][2] = cos(rotation.y);
+	matRotY.m[3][3] = 1;
+
+	//各軸の回転行列を合成
+	matRot = matRotZ;
+	matRot *= matRotX;
+	matRot *= matRotY;
+
+	return matRot;
+}
+//平行移動
+Matrix4 CreateMatTrans(Vector3 translation) {
+	//平行移動を宣言
+	Matrix4 matTrans = MathUtility::Matrix4Identity();
+
+	matTrans.m[0][0] = 1;
+	matTrans.m[1][1] = 1;
+	matTrans.m[2][2] = 1;
+	matTrans.m[3][0] = translation.x;
+	matTrans.m[3][1] = translation.y;
+	matTrans.m[3][2] = translation.z;
+	matTrans.m[3][3] = 1;
+
+	return matTrans;
+}
+//単位行列
+Matrix4 CreateIdentityMatrix()
+{
+	Matrix4 Identity;
+	//単位行列を代入
+	Identity.m[0][0] = 1.0f;
+	Identity.m[1][1] = 1.0f;
+	Identity.m[2][2] = 1.0f;
+	Identity.m[3][3] = 1.0f;
+	return Identity;
+}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	//軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debugCamera_->GetViewProjection());
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+	//X,Y,Z 方向のスケーリングを設定
+	worldTransform_.scale_ = { 5.0f,5.0f,5.0f };
+
+	// X,Y,Z軸周りの回転角を設定
+	worldTransform_.rotation_ = { Angle(45.0f),Angle(45.0f),Angle(0.0f)};
+	
+	//X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = { 10,10,10 };
+
+	//単位行列を代入
+	worldTransform_.matWorld_=CreateIdentityMatrix();
+	
+	//ワールド座標に掛け算して代入
+	worldTransform_.matWorld_ *= CreateMatScale(worldTransform_.scale_);
+	worldTransform_.matWorld_ *= CreateMatRot(worldTransform_.rotation_);
+	worldTransform_.matWorld_ *= CreateMatTrans(worldTransform_.translation_);
+
+	//行列の転送
+	worldTransform_.TransferMatrix();
+}
+void GameScene::Update() {
+	//デバッグカメラの更新
+	debugCamera_->Update();
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	//PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 0), Vector3(80, 30, 100), Vector4(255, 255, 255, 255));
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>f
+	/// 3Dモデル描画
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// デバッグテキストの描画
+	debugText_->DrawAll(commandList);
+	//
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
